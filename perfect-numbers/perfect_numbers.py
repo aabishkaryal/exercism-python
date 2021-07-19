e35c3eb029cad681b19c360a63b7d6d6c381435e
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-07-19 17:48:59 +0000

    Refactored: Improved perfect_numbers

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -1,4 +1,7 @@
-def classify(number):
+from typing import Set
+
+
+def classify(number: int) -> str:
     if number > 0:
         factors = getFactors(number)
         s = sum(factors)
@@ -12,9 +15,13 @@ def classify(number):
         raise ValueError("positive integer required")
 
 
-def getFactors(n):
-    factors = []
-    for i in range(1, n):
+def getFactors(n: int) -> Set[int]:
+    if n <= 1:
+        return []
+    factors = {1, }
+    limit = int(n ** 0.5)+1
+    for i in range(2, limit):
         if n % i == 0:
-            factors.append(i)
+            factors.add(i)
+            factors.add(n/i)
     return factors

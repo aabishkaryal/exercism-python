5cbc561c0881f0a112fb5d45adf7be9e62c4a978
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-07-22 20:36:30 +0545

    Refactor: cached plants to self.student_plants, changed convert to plants dictionary

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -1,28 +1,42 @@
-from typing import List
+from typing import Dict, List
 from collections import defaultdict
+
+# Convert plant first letter to full plant name
+plants = {
+    "G": "Grass",
+    "C": "Clover",
+    "R": "Radishes",
+    "V": "Violets",
+}
 
 
 class Garden:
     def __init__(self, diagram: str,
                  students: List[str] = ["Alice", "Bob", "Charlie", "David", "Eve", "Fred",
                                         "Ginney", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"]):
-        self.rows = [[self.convert(plant) for plant in list(row)]
+        # Split diagram to proper row with full plant name
+        self.rows = [[plants[p] for p in list(row)]
                      for row in diagram.splitlines()]
+        # Sort the students alphabetically
         self.students = sorted(students)
 
-    def plants(self, student: str) -> List[str]:
+        # Determine which student is responsible for which plant and store in dictionary.
+        self.student_plants: Dict[str, List[str]] = {}
+        for student in self.students:
+            self.student_plants[student] = self.__getPlants(student)
+
+    def __getPlants(self, student: str) -> List[str]:
+        '''
+        Returns the plants for which the student is responsible for taking care of.
+        '''
         i = self.students.index(student)
         result: List[str] = []
         for row in self.rows:
             result.extend(row[2*i:2*i+2])
         return result
 
-    def convert(self, p: str) -> str:
-        if p == "G":
-            return "Grass"
-        elif p == "C":
-            return "Clover"
-        elif p == "R":
-            return "Radishes"
-        elif p == "V":
-            return "Violets"
+    def plants(self, student: str) -> List[str]:
+        '''
+        Returns the plants for which the student is responsible for taking care of.
+        '''
+        return self.student_plants[student]
